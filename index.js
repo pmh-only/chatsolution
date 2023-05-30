ce3279a18e2313b2216f08ab51690f779e0879f3
pmh-only <opensource@pmh.codes>
2023-05-30 16:19:31 +0900

    feat: remove client

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const express = require('express')
-const app = express()
 const http = require('http')
-const server = http.createServer(app)
 const { Server } = require("socket.io")
+
+const server = http.createServer()
 const io = new Server(server, {
-  cors: {
-    origin: '*'
-  }
-})
-const cors = require('cors')
-const { readFileSync } = require('fs')
-
-app.use(cors())
-app.get('/', (_, res) => {
-  res.set('Content-Type', 'text/javascript')
-  res.send(readFileSync('./client.js', 'utf-8'))
+  cors: { origin: '*' }
 })
 
-io.on('connection', (socket) => {
-  socket.join('main')
-  
-  socket.on('broadcast', (data, ack) => {
-    io.emit('broadcast', data)
-    ack?.()
-  })
-})
+io.on('connection', (socket) =>
+  socket.on('broadcast', (data, ack) =>
+    io.emit('broadcast', data) && ack?.()))
 
 server.listen(80)
