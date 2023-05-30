4623d6f83855e354370adbd87eb71f5ae459d753
pmh-only <opensource@pmh.codes>
2023-05-30 14:52:36 +0900

    feat: add room client

--- client.js
+++ client.js
@@ -2,13 +2,15 @@ import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js'
 
 const socket = io('wss://chatsolution.shutupandtakemy.codes')
 let author = '익명의 누군가'
+let room = undefined
 
 socket.on('connect', () => {
   console.log({ type: 'room connected', success: true })
 })
 
 socket.on('chat', (data) => {
-  console.log(data)
+  if (data.room === undefined || data.room === room)
+    console.log(data)
 })
 
 window.a = ([data]) => {
@@ -17,6 +19,11 @@ window.a = ([data]) => {
 }
 
 window.c = ([chat]) => {
-  socket.emit('chat', { success: true, author, data: chat })
+  socket.emit('chat', { success: true, room, author, data: chat })
   return { type: 'message sent', success: true }
+}
+
+window.r = ([roomId]) => {
+  room = roomId
+  return { type: 'room changed', success: true }
 }
