ed0bd0250ec043d759ca616667306ba41ac88afa
pmh-only <opensource@pmh.codes>
2023-06-05 11:30:51 +0900

    feat: remove utf8 validation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import WebSocket, { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({ port: 80 })
+const wss = new WebSocketServer({ port: 80, skipUTF8Validation: true })
 
 wss.on('connection', (ws) =>
   ws.on('message', (data) =>
     wss.clients.forEach((client) =>
       client.readyState === WebSocket.OPEN &&
-        client.send(data))))
+        client.send(data, { binary: true }))))
